a4917fd75e00c57ad52d210a92715d84c2649d51
LJH <140390243+MengMian02@users.noreply.github.com>
2026-03-12 14:13:27 +0000

    update

--- return_after_jump/data_collection_to_db.py
+++ return_after_jump/data_collection_to_db.py
@@ -1,7 +1,8 @@
 import duckdb
 import pandas as pd
 from pathlib import Path
-
+import yfinance as yf
+from numpy.linalg.lapack_lite import dgelsd
 
 db_path = Path('data\\data.db')
 spy_holdings_url = "https://www.ssga.com/us/en/intermediary/etfs/library-content/products/fund-data/etfs/us/holdings-daily-us-en-spy.xlsx"
@@ -13,7 +14,7 @@ def get_db_connection(db_path):
     return duckdb.connect(str(db_path))
 
 
-def create_table(con, table_name):
+def create_ticker_table(con, table_name):
     con.execute(f"""
     CREATE TABLE IF NOT EXISTS {table_name} (
     ticker TEXT PRIMARY KEY,
@@ -42,10 +43,48 @@ def validate_rename_col(df, mapping):
     return df.rename(columns=mapping)
 
 
-def load_df_into_db(con, df, table_name, temp_view='temp_df'):
+def load_firm_df_into_db(con, df, table_name, temp_view='temp_df'):
     con.register(temp_view, df)
     con.execute(f'''
         INSERT OR IGNORE INTO {table_name} (ticker, company)
         SELECT ticker, company
+        FROM {temp_view}''')
+    con.unregister(temp_view)
+
+
+def create_price_table(con, table_name='prices_daily'):
+    con.execute(f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        ticker TEXT,
+        date DATE, 
+        open FLOAT,
+        high FLOAT,
+        low FLOAT,
+        close FLOAT,
+        PRIMARY KEY(ticker, date)
+        """)
+
+
+def get_tickers(con, ticker_table):
+    rows = con.execute(f"""
+        SELECT ticker FROM {ticker_table}
+        ORDER BY ticker""").fetchall()
+    return [row[0] for row in rows]
+
+
+def get_price_history(ticker, size, period, interval='1d'):
+    df = yf.download(ticker, period=period, interval=interval)
+    df.columns = [col[0] if isinstance(col, tuple) else col for col in df.columns]
+    df = df.reset_index()
+    df['ticker'] = ticker
+    df['size'] = size
+    return df
+
+
+def load_price_df_into_db(con, df, table_name, temp_view='temp_prices'):
+    con.register(temp_view, df)
+    con.execute(f'''
+        INSERT OR IGNORE INTO {table_name} (ticker, date, Close, High, Low, Open, Volume, size)
+        SELECT ticker, date, Close, High, Low, Open, Volume, size
         FROM {temp_view}''')
     con.unregister(temp_view)
