935ee71a5d1c8a043e7aa1e503f7ffae0a16d3b5
LJH <140390243+MengMian02@users.noreply.github.com>
2026-03-12 22:08:57 +0000

    update

--- return_after_jump/main.py
+++ return_after_jump/main.py
@@ -2,14 +2,13 @@ import pandas as pd
 import data_preparation
 from simple_market_analysis.return_after_jump.utils import identify_jumps, get_post_jump_days, plot_hist, jump_analysis
 
-ticker = 'FENC'
 t = 1
 
 
 def main(ticker, dir, t):
-    data_source = 'D:\\Jiheng\\Python Projects\\PythonProject\\simple_market_analysis\\return_after_jump\\data\\' + ticker + '.csv'
-    raw_data = pd.read_csv(data_source)
-    cleaned_data = data_preparation.clean_data(raw_data)
+    # data_source = 'D:\\Jiheng\\Python Projects\\PythonProject\\simple_market_analysis\\return_after_jump\\data\\' + ticker + '.csv'
+    # raw_data = pd.read_csv(data_source)
+    # cleaned_data = data_preparation.clean_data(raw_data)
     data_with_vol = data_preparation.prepare_variables(cleaned_data)
     data_with_jump = identify_jumps(data_with_vol, dir)
     jump_days = get_post_jump_days(data_with_jump, t)
@@ -18,3 +17,6 @@ def main(ticker, dir, t):
 
 
 # plot_hist(jump_days['Return'])
+
+
+
